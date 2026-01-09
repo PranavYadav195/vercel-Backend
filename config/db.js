a91272d2cb84ccd103908bec392758ca2740afb4
Your Name <you@example.com>
2026-01-09 11:40:10 +0530

    kya re

--- config/db.js
+++ config/db.js
@@ -1,22 +1,13 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
 const connectDB = async () => {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.URL).then((mongoose) => {
-      return mongoose;
-    });
+  try {
+    await mongoose.connect("mongodb+srv://pranavy187_db_user:root@cluster0.nddg2if.mongodb.net/crudoperation");
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
   }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
 };
 
 export default connectDB;
--- index.js
+++ index.js
@@ -15,4 +15,8 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 } );  
 
-export default app;
+app.listen(process.env.PORT || 5000, () => { 
+  console.log(`Server is running on port ${process.env.PORT || 5000}`); 
+  connectDB();
+}
+);
